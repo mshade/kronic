aa43919af41c2138b18d4e24197cff1ee55198a6
Mike Shade <mshade@mshade.org>
2023-08-29 00:51:27 -0400

    :art: black formatting and comments

--- app.py
+++ app.py
@@ -1,41 +1,59 @@
 from flask import Flask, request, render_template, redirect
 import yaml
 
-from kron import getCronJobs, getNamespaces, getJobs, getCronJob, getPods, getPodLogs, toggleCronJob, triggerCronJob, updateCronJob, deleteCronJob
+from kron import (
+    getCronJobs,
+    getNamespaces,
+    getJobs,
+    getCronJob,
+    getPods,
+    getPodLogs,
+    toggleCronJob,
+    triggerCronJob,
+    updateCronJob,
+    deleteCronJob,
+)
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='static')
+app = Flask(__name__, static_url_path="", static_folder="static")
+
 
 @app.route("/")
 def index():
     cronjobs = getCronJobs()
     namespaces = {}
+    # Count cronjobs per namespace
     for cronjob in cronjobs:
         namespaces[cronjob["namespace"]] = namespaces.get(cronjob["namespace"], 0) + 1
 
     return render_template("index.html", namespaces=namespaces)
 
+
 @app.route("/namespaces/<name>")
 def namespaces(name):
     cronjob_names = getCronJobs(name)
-    cronjobs = [getCronJob(namespace=name, cronjob_name=cronjob["name"]) for cronjob in cronjob_names]
+    cronjobs = [
+        getCronJob(namespace=name, cronjob_name=cronjob["name"])
+        for cronjob in cronjob_names
+    ]
     for cron in cronjobs:
         jobs = getJobs(namespace=name, cronjob_name=cron["metadata"]["name"])
         cron["jobs"] = jobs
         for job in cron["jobs"]:
-            job["pods"] = getPods(cron["metadata"]["namespace"], job["metadata"]["name"])
+            job["pods"] = getPods(
+                cron["metadata"]["namespace"], job["metadata"]["name"]
+            )
 
     return render_template("namespaceJobs.html", cronjobs=cronjobs, namespace=name)
 
-@app.route("/namespaces/<namespace>/cronjobs/<cronjob_name>", methods = ['GET', 'POST'])
+
+@app.route("/namespaces/<namespace>/cronjobs/<cronjob_name>", methods=["GET", "POST"])
 def cronjobView(namespace, cronjob_name):
-    if request.method == 'POST':
-        edited_cronjob = yaml.safe_load(request.form['yaml'])
+    if request.method == "POST":
+        edited_cronjob = yaml.safe_load(request.form["yaml"])
         cronjob = updateCronJob(namespace, edited_cronjob)
     else:
         cronjob = getCronJob(namespace, cronjob_name)
-    
+
     del cronjob["status"]
     del cronjob["metadata"]["uid"]
     del cronjob["metadata"]["resourceVersion"]
@@ -43,20 +61,26 @@ def cronjobView(namespace, cronjob_name):
     cronjob_yaml = yaml.dump(cronjob)
     return render_template("cronjob.html", cronjob=cronjob, yaml=cronjob_yaml)
 
-@app.route("/api/namespaces/<namespace>/cronjobs/<cronjob_name>/delete", methods = ['POST'])
+
+@app.route(
+    "/api/namespaces/<namespace>/cronjobs/<cronjob_name>/delete", methods=["POST"]
+)
 def deleteJobEndpoint(namespace, cronjob_name):
     deleted = deleteCronJob(namespace, cronjob_name)
     return redirect(f"/namespaces/{namespace}", code=302)
+
 
 @app.route("/api/")
 def allCronJobs():
     jobs = getCronJobs()
     return jobs
 
+
 @app.route("/api/namespaces/")
 def apiNamespaces():
     namespaces = getNamespaces()
     return namespaces
+
 
 @app.route("/api/namespaces/<name>/cronjobs")
 @app.route("/api/namespaces/<name>")
@@ -64,40 +88,51 @@ def namespace(name):
     jobs = getCronJobs(name)
     return jobs
 
+
 @app.route("/api/namespaces/<namespace>/cronjobs/<cronjob_name>")
 def showCronJob(namespace, cronjob_name):
     cronjob = getCronJob(namespace, cronjob_name)
     return cronjob
 
-@app.route("/api/namespaces/<namespace>/cronjobs/<cronjob_name>/suspend", methods = ['GET', 'POST'])
+
+@app.route(
+    "/api/namespaces/<namespace>/cronjobs/<cronjob_name>/suspend",
+    methods=["GET", "POST"],
+)
 def getSetSuspended(namespace, cronjob_name):
-    if request.method == 'GET':
+    if request.method == "GET":
         """Return the suspended status of the <cronjob_name>"""
         cronjob = getCronJob(namespace, cronjob_name)
         return cronjob
-    if request.method == 'POST':
+    if request.method == "POST":
         """Toggle the suspended status of <cronjob_name>"""
         cronjob = toggleCronJob(namespace, cronjob_name)
         return cronjob
 
-@app.route("/api/namespaces/<namespace>/cronjobs/<cronjob_name>/trigger", methods = ['POST'])
+
+@app.route(
+    "/api/namespaces/<namespace>/cronjobs/<cronjob_name>/trigger", methods=["POST"]
+)
 def triggerNewJob(namespace, cronjob_name):
     """Manually trigger a job from <cronjob_name>"""
     cronjob = triggerCronJob(namespace, cronjob_name)
     return cronjob
 
-@app.route('/api/namespaces/<namespace>/jobs', defaults={'cronjob_name': None})
-@app.route('/api/namespaces/<namespace>/jobs/<cronjob_name>')
+
+@app.route("/api/namespaces/<namespace>/jobs", defaults={"cronjob_name": None})
+@app.route("/api/namespaces/<namespace>/jobs/<cronjob_name>")
 def showJobs(namespace, cronjob_name):
     jobs = getJobs(namespace, cronjob_name)
     return jobs
 
-@app.route('/api/namespaces/<namespace>/pods')
+
+@app.route("/api/namespaces/<namespace>/pods")
 def showPods(namespace):
     pods = getPods(namespace)
     return pods
 
-@app.route('/api/namespaces/<namespace>/pods/<pod_name>/logs')
+
+@app.route("/api/namespaces/<namespace>/pods/<pod_name>/logs")
 def showPodLogs(namespace, pod_name):
     logs = getPodLogs(namespace, pod_name)
     return logs
--- kron.py
+++ kron.py
@@ -7,15 +7,17 @@ try:
     # Load configuration inside the Pod
     config.load_incluster_config()
 except ConfigException:
-    # Load configuration for testing
+    # Load configuration from KUBECONFIG
     config.load_kube_config()
 
-# Create the Apis
+# Create the Api clients
 v1 = client.CoreV1Api()
 batch = client.BatchV1Api()
 generic = client.ApiClient()
 
+
 def _itemFields(response, fields=["name"]):
+    """Filter the response down to only listed fields"""
     itemFields = []
     for item in response.items:
         x = {}
@@ -25,14 +27,16 @@ def _itemFields(response, fields=["name"]):
 
     return itemFields
 
+
 def _cleanObject(api_object):
-    ### Return JSON without managedFields blob
+    """Convert API object to JSON and strip managedFields"""
     object = generic.sanitize_for_serialization(api_object)
     object["metadata"].pop("managedFields")
     return object
 
+
 def _hasLabel(api_object, k, v):
-    ### Return True/False if a label key and value match
+    """Return True if a label is present with specified value"""
     metadata = api_object["metadata"]
     if "labels" in metadata:
         print("object has labels")
@@ -43,14 +47,16 @@ def _hasLabel(api_object, k, v):
                 return True
     return False
 
+
 def getCronJobs(namespace=""):
     if namespace == "":
         cronjobs = batch.list_cron_job_for_all_namespaces()
     else:
         cronjobs = batch.list_namespaced_cron_job(namespace=namespace)
-    
+
     fields = ["name", "namespace"]
     return _itemFields(cronjobs, fields)
+
 
 def getCronJob(namespace, cronjob_name):
     try:
@@ -60,9 +66,11 @@ def getCronJob(namespace, cronjob_name):
 
     return _cleanObject(cronjob)
 
+
 def getNamespaces():
     namespaces = v1.list_namespace()
     return _itemFields(namespaces)
+
 
 def getJobs(namespace, cronjob_name):
     jobs = batch.list_namespaced_job(namespace=namespace)
@@ -80,6 +88,7 @@ def getJobs(namespace, cronjob_name):
 
     return filtered
 
+
 def getPods(namespace, job_name=None):
     all_pods = v1.list_namespaced_pod(namespace=namespace)
     cleaned = [_cleanObject(pod) for pod in all_pods.items]
@@ -96,35 +105,40 @@ def getPods(namespace, job_name=None):
 
     return pods
 
+
 def getPodLogs(namespace, pod_name):
-    logs = v1.read_namespaced_pod_log(pod_name, namespace, tail_lines=1000, timestamps=True)
+    logs = v1.read_namespaced_pod_log(
+        pod_name, namespace, tail_lines=1000, timestamps=True
+    )
     return logs
+
 
 def triggerCronJob(namespace, cronjob_name):
     cronjob = batch.read_namespaced_cron_job(name=cronjob_name, namespace=namespace)
     job = cronjob.spec.job_template
     date_stamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S-%f")
     # Set a unique name that indicates this is a manual invocation
-    job.metadata.name = str(job.metadata.name + '-manual-' + date_stamp)[:64]
+    job.metadata.name = str(job.metadata.name + "-manual-" + date_stamp)[:64]
     # Set a label to identify jobs created by kron
     job.metadata.labels = {
         "kron.mshade.org/manually-triggered": "true",
-        "kron.mshade.org/created-from": cronjob_name
-        }
+        "kron.mshade.org/created-from": cronjob_name,
+    }
 
     trigger_job = batch.create_namespaced_job(body=job, namespace=namespace)
     return _cleanObject(trigger_job)
 
 
 def toggleCronJob(namespace, cronjob_name):
-    suspended_status = batch.read_namespaced_cron_job_status(name=cronjob_name, namespace=namespace)
-    patch_body = {
-        "spec": {
-            "suspend": not suspended_status.spec.suspend
-        }
-    }
-    cronjob = batch.patch_namespaced_cron_job(name=cronjob_name, namespace=namespace, body=patch_body)
+    suspended_status = batch.read_namespaced_cron_job_status(
+        name=cronjob_name, namespace=namespace
+    )
+    patch_body = {"spec": {"suspend": not suspended_status.spec.suspend}}
+    cronjob = batch.patch_namespaced_cron_job(
+        name=cronjob_name, namespace=namespace, body=patch_body
+    )
     return _cleanObject(cronjob)
+
 
 def updateCronJob(namespace, spec):
     name = spec["metadata"]["name"]
@@ -134,8 +148,9 @@ def updateCronJob(namespace, spec):
         cronjob = batch.create_namespaced_cron_job(namespace, spec)
     return _cleanObject(cronjob)
 
+
 def deleteCronJob(namespace, cronjob_name):
-    try: 
+    try:
         deleted = batch.delete_namespaced_cron_job(cronjob_name, namespace)
     except ApiException:
         return False
