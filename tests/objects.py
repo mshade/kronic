9f6982a6cd1cae018b4c4fe281efcdd20ff3f42c
Mike Shade <mshade@mshade.org>
2023-09-11 13:14:34 -0400

    :recycle: refactor objects into functions for creation

--- tests/objects.py
+++ tests/objects.py
@@ -2,35 +2,43 @@ from kubernetes import client
 
 
 ## Create API Objects for testing
-container = client.V1Container(name="test", image="busybox", command=["echo", "hello"])
+
 labels = {"app": "test"}
-pod_template = client.V1PodTemplateSpec(
-    metadata=client.V1ObjectMeta(labels=labels),
-    spec=client.V1PodSpec(restart_policy="Never", containers=[container]),
-)
-
-job_spec = client.V1JobSpec(template=pod_template)
-
-job = client.V1Job(
-    api_version="batch/v1",
-    kind="Job",
-    metadata=client.V1ObjectMeta(name="hello1", labels=labels),
-    spec=job_spec,
-)
 
 
-def create_cronjob(name):
+def create_pod_spec(name="test"):
+    container = client.V1Container(
+        name=name, image="busybox", command=["echo", "hello"]
+    )
+    pod = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels=labels),
+        spec=client.V1PodSpec(restart_policy="Never", containers=[container]),
+    )
+    return pod
+
+
+def create_job(name="test"):
+    job_spec = client.V1JobSpec(template=create_pod_spec())
+    return client.V1Job(
+        api_version="batch/v1",
+        kind="Job",
+        metadata=client.V1ObjectMeta(name=name, labels=labels),
+        spec=job_spec,
+    )
+
+
+def create_cronjob(name="test"):
     cronjob = client.V1CronJob(
         api_version="batch/v1",
         kind="CronJob",
         metadata=client.V1ObjectMeta(name=name, namespace="test", labels=labels),
-        spec=client.V1CronJobSpec(schedule="* * * * *", job_template=job),
+        spec=client.V1CronJobSpec(schedule="* * * * *", job_template=create_job()),
     )
     return cronjob
 
 
-jobs = ["first", "second", "third", "fourth", "fifth"]
-
-cronjobList = client.V1CronJobList(
-    api_version="batch/v1", items=[create_cronjob(job) for job in jobs]
-)
+def create_cronjob_list():
+    jobs = ["first", "second", "third", "fourth", "fifth"]
+    return client.V1CronJobList(
+        api_version="batch/v1", items=[create_cronjob(job) for job in jobs]
+    )
